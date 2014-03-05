af1490d497fe6a0f52a1dcd86f6aa542ea134955
James Faske <jamesfaske@yahoo.com>
2014-03-05 15:58:15 -0600

    Initial

---
+++ ReportSpam/Ribbon1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ReportSpam
+{
+    partial class ReportSpam : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public ReportSpam()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportSpam));
+            this.Home = this.Factory.CreateRibbonTab();
+            this.Custom = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.Home.SuspendLayout();
+            this.Custom.SuspendLayout();
+            // 
+            // Home
+            // 
+            this.Home.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.Home.ControlId.OfficeId = "TabMail";
+            this.Home.Groups.Add(this.Custom);
+            this.Home.Label = "TabMail";
+            this.Home.Name = "Home";
+            // 
+            // Custom
+            // 
+            this.Custom.Items.Add(this.button1);
+            this.Custom.Label = "Custom";
+            this.Custom.Name = "Custom";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Label = "Report Spam";
+            this.button1.Name = "button1";
+            this.button1.ScreenTip = "Report selected email as spam";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // ReportSpam
+            // 
+            this.Name = "ReportSpam";
+            this.RibbonType = "Microsoft.Outlook.Explorer";
+            this.Tabs.Add(this.Home);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.Home.ResumeLayout(false);
+            this.Home.PerformLayout();
+            this.Custom.ResumeLayout(false);
+            this.Custom.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab Home;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Custom;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal ReportSpam Ribbon1
+        {
+            get { return this.GetRibbon<ReportSpam>(); }
+        }
+    }
+}
---
+++ ReportSpam/Ribbon1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Office = Microsoft.Office.Core;
+using Outlook = Microsoft.Office.Interop.Outlook;
+
+namespace ReportSpam
+{
+    public partial class ReportSpam
+    {
+        private string path = @"c:\ReportSpamOfficeAddIn\";
+        private string fileName = "settings.ini";
+
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            //if the settings file does not exist, create it
+            if (!File.Exists(path+fileName))
+            {
+                CreateFile();
+            }
+        }
+
+        private void CreateFile()
+        {
+            try
+            {
+                Directory.CreateDirectory(path);
+                File.WriteAllText(path + fileName, "email@address.com");
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show(@"Error creating/writing to email settings file "+path+fileName);
+            }
+        }
+
+        private string ReadFile()
+        {
+            try
+            {
+                return File.ReadAllText(path + fileName);
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("Unable to read settings file "+path+fileName);
+                return "";
+            }
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Outlook.Application application = new Outlook.Application();
+            Outlook.NameSpace ns = application.GetNamespace("MAPI");
+
+            try
+            {
+                //get selected mail item
+                Object selectedObject = application.ActiveExplorer().Selection[1];
+                Outlook.MailItem selectedMail = (Outlook.MailItem)selectedObject;
+
+                //create message
+                Outlook.MailItem newMail = application.CreateItem(Outlook.OlItemType.olMailItem) as Outlook.MailItem;
+                newMail.Recipients.Add(ReadFile());
+                newMail.Subject = "SPAM";
+                newMail.Attachments.Add(selectedMail, Microsoft.Office.Interop.Outlook.OlAttachmentType.olEmbeddeditem);
+
+                newMail.Send();
+                selectedMail.Delete();
+
+                System.Windows.Forms.MessageBox.Show("Spam notification has been sent.");
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("You must select a message to report.");
+            }
+        }
+    }
+}
